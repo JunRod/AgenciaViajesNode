0ffd462836c3fabbdfe8a2a9e103d00149e11749
JunRod <juniormen.12321@gmail.com>
2022-12-14 12:36:59 -0500

    nuevo

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@
 import Sequelize from "sequelize";
 import dotenv from "dotenv/config" //Con dotenv, cuando el usuario entre a este archivo desde github, solo podrá ver el nombre de las variables pero no las credenciales originales, es decir los valores.
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const db = new Sequelize(process.env.DB_NAME , process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    post: "3306",
+    port: "3306",
     dialect: "mysql",
     define: {
         timestamps: false
